d7c6b789d8e6860e8555283e58aa422bf708ffdb
Carlos <carlosejimenezh95@gmail.com>
2022-02-21 09:12:59 -0600

    cambiar la condicion para la clase activo en navbar

--- header.php
+++ header.php
@@ -21,10 +21,12 @@
             <a href="solidworks.php" class="w3-bar-item w3-hide-small <?php if(isset($sec) && $sec==2){ echo "activo";}?>">SOLIDWORKS</a>
             <a href="camworks.php" class="w3-bar-item w3-hide-small <?php if(isset($sec) && $sec==3){ echo "activo";}?>">CAMWORKS</a>
             <a href="capacitacion.php" class="w3-bar-item w3-hide-small <?php if(isset($sec) && $sec==4){ echo "activo";}?>">CAPACITACIÓN</a>
-            <a href="3dexperience.php" class="w3-bar-item w3-hide-small <?php if(isset($sec) && $sec==4){ echo "activo";}?>">3D EXPERIENCE</a>
-            <a href="contacto.php" class="w3-bar-item w3-hide-small <?php if(isset($sec) && $sec==4){ echo "activo";}?>">CONTACTO</a>
+            <a href="3dexperience.php" class="w3-bar-item w3-hide-small <?php if(isset($sec) && $sec==5){ echo "activo";}?>">3D EXPERIENCE</a>
+            <a href="contacto.php" class="w3-bar-item w3-hide-small <?php if(isset($sec) && $sec==6){ echo "activo";}?>">CONTACTO</a>
             <button href="javascript:void(0)" class="w3-bar-item w3-right w3-hide-large w3-hide-medium" 
-            style="font-size: 1.5em; margin:0; padding-right:20px; background-color:white" onclick="myFunction()">&#9776;</button>
+            style="font-size: 1.5em; margin:0; padding-right:20px; background-color:white" onclick="myFunction()">
+                &#9776;
+            </button>
         </div>
     </nav>
 </header>
