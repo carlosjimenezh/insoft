d22f7a419e15ebb940150f0cbb179a10fb4ec95b
Carlos <carlosejimenezh95@gmail.com>
2022-02-22 11:56:23 -0600

    crear secciones de paquetes

--- solidworks.php
+++ solidworks.php
@@ -25,16 +25,49 @@
 
     <section class="contenido" style="max-width:1350px">
         <div style="border: 1px solid black;">
-            <div class="contenido">
-                prueba
+            <div class="contenido w3-row">
+                <div class="w3-half mitadizq">
+                    <h2>CAD MECÁNICO EN 3D</h2>
+                    <h2><strong>STANDAR</strong></h2>
+                    Ofrece acceso a todas las herramientas necesarias para realizar superficies complejas, 
+                    chapas desplegadas de chapa metálica y ensamblajes soldados estructurales. Además, incluye 
+                    asistentes para automatizar los diseños, realizar análisis de tensión y determinar el 
+                    impacto medioambiental de los componentes. <br>
+                    <a href="" class="btn">Más información</a>
+                </div>
+                <div class="w3-half mitadder">
+                    <img src="" alt="">
+                </div>
             </div>
         </div>
-        <div class="contenido">
-            prueba
+        <div class="contenido w3-row">
+            <div class="w3-half mitadder">
+                <img src="" alt="">
+            </div>
+            <div class="w3-half mitadizq">
+                <h2>CAD MECÁNICO EN 3D</h2>
+                <h2><strong>PROFESIONAL</strong></h2>
+                SolidWorks Professional contiene bibliotecas de piezas y tornillería, herramientas para 
+                utilizar la geometría importada y para buscar errores en los diseños. Además, SolidWorks 
+                Professional ofrece un sistema integrado de gestión de datos que almacena de forma segura 
+                toda la información del proyecto y registra todas las modificaciones del diseño. <br>
+                <a href="" class="btn">Más información</a>
+            </div>
         </div>
         <div style="border: 1px solid black;">
-            <div class="contenido">
-                prueba
+            <div class="contenido w3-row">
+                <div class="w3-half mitadizq">
+                        <h2>CAD MECÁNICO EN 3D</h2>
+                        <h2><strong>PREMIUM</strong></h2>
+                        SOLIDWORKS Premium es una solución de diseño en 3D completa que se suma a las 
+                        funciones de SolidWorks Professional junto con las potentes herramientas de simulación, 
+                        movimiento y validación de diseños, así como la funcionalidad avanzada de trazado de 
+                        recorridos de tuberías y cables, ingeniería inversa, entre otras. <br>
+                        <a href="" class="btn">Más información</a>
+                    </div>
+                    <div class="w3-half mitadder">
+                        <img src="" alt="">
+                </div>
             </div>
         </div>
     </section>
