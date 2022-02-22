15ade44894ad7e96883b45dedce4f24ec40f4492
Carlos <carlosejimenezh95@gmail.com>
2022-02-22 09:28:29 -0600

    crear página solidworks

---
+++ solidworks.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Solidworks</title>
+    <link rel="stylesheet" href="w3pro.css">
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <?php $sec=1; ?>
+    <?php include("header.php"); ?>
+    <div class="banner-principal" style="height: 600px; background-color:aliceblue">
+        <div class="contenido">
+            prueba
+        </div>
+    </div>
+
+    <section id="productos">
+        <div class="w3-center contenido" style="max-width: 700px;">
+            <h1>PRODUCTOS Y SOLUCIONES</h1>
+            Contamos con soluciones eficaces e intuitivas para innovar y abarcar todos 
+            los aspectos del proceso de desarrollo de productos, sin importar el tamaño de su empresa.
+            <br>
+            <button>Más información</button>
+        </div>
+        <div class="w3-row contenido w3-center">
+            <div class="w3-third mitadizq">
+                <img src="" alt="">
+                <h2>SOLIDWORKS</h2>
+                <strong>DISEÑA</strong> <br>
+                Es el mejor Software CAD de diseño asistido por computadora para modelado mecánico
+                en 3D, te permite gracias a sus herramientas innovar y desarollar tus propios productos.
+            </div>
+            <div class="w3-third mitadcentro">
+            <img src="" alt="">
+                <h2>CAMWorks</h2>
+                <strong>SIMULA</strong> <br>
+                La conexión entre el diseño y mecanizado CNC para crear código G y la simulación de 
+                maquinados CNC, integrado totalmente a SOLIDWORKS será mas fácil las actualizaciones de diseño.
+            </div>
+            <div class="w3-third mitadder">
+            <img src="" alt="">
+                <h2>Capacitación</h2>
+                <strong>APRENDE</strong> <br>
+                Visita nuestros distintos centros de capacitación equipadas y acondicionadas SOLIDWORKS 
+                en México, Guadalajara y Monterrey, ven y descubre porque somos ideal para ti.
+            </div>
+        </div>
+    </section>
+
+
+    <section id="paralax" style="background-color: aliceblue; height:400px; position:relative">
+        <div class="w3-display-middle txtblanco">
+            PARALAX
+        </div>
+    </section>
+
+    <section id="capacitacion">
+        <div class="w3-center contenido" style="max-width: 700px;">
+            <h1>CAPACITACIÓN</h1>
+            Contamos con cursos desde nivel básico y más de 15 especialidades que te 
+            ayudarán a realizar tu trabajo de manera exitosa, cumpliendo las demandas del mercado laboral actual.
+            <br>
+            <button>Contactar</button>
+        </div>
+        <div class="contenido w3-center">
+            <div class="w3-row">
+                <div class="w3-third mitadizq">
+                    <img src="" alt="">
+                    <h2>CLASE VIRTUAL</h2>
+                    Es el mejor Software CAD de diseño asistido por computadora para modelado mecánico
+                    en 3D, te permite gracias a sus herramientas innovar y desarollar tus propios productos.
+                </div>
+                <div class="w3-third mitadcentro">
+                    <img src="" alt="">
+                    <h2>FECHAS</h2>
+                    <b>HORARIO:</b> <br>
+                    <b>Lunes a Viernes</b> <br> 
+                    A partir de las 7 AM hasta las 7 PM <br>
+                    <b>Sábados y Domingos</b> <br>
+                    A partir de las 9 AM hasta las 2 PM.
+                </div>
+                <div class="w3-third mitadder">
+                    <img src="" alt="">
+                    <h2>TEMARIOS</h2>
+                    Conoce los cursos de SOLIDWORKS que tenemos para ti junto con el plan de estudios, 
+                    duración, examenes de ubicación y los requisitos para mejorar tu aprendizaje.
+                </div>
+            </div>
+            <div class="w3-row">
+                <div class="w3-third mitadizq">
+                    <img src="" alt="">
+                    <h2>CURSOS</h2>
+                    Contamos con una amplia variedad de cursos que te ayudarán a desempeñarte 
+                    de mejor manera en el ámbito laboral.
+                </div>
+                <div class="w3-third mitadcentro">
+                    <img src="" alt="">
+                    <h2>DIPLOMADOS</h2>
+                    Nuestros cursos y diplomados SOLIDWORKS tienen validez curricular lo cual 
+                    tiene genera interés para la industria.
+                </div>
+                <div class="w3-third mitadder">
+                    <img src="" alt="">
+                    <h2>CERTIFICIONES</h2>
+                    Te permitirán diseñar, gestionar, analizar, competir y sobresalir, sobre los 
+                    demás ingenieros a nivel mundial.
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section id="cuadro-rojo">
+        <div style="height: 200px; background-color:red">
+            <div class=" contenido txtblanco">
+                Conviertete en un experto
+                <img src="" alt="">
+            </div>
+        </div>
+    </section>
+    
+
+    <section id="centro-capacitacion">
+        <div class="contenido w3-row w3-center" style="max-width: 700px;">
+            <h1>CENTROS DE CAPACITACIÓN</h1>
+            <div class="w3-half mitadizq">
+                <h2>Guadalajara</h2>
+                Av. Cristóbal Colón 2706 <br>
+                Col. Jardines de la Cruz  <br>
+                01 (33) 3124 3922
+            </div>
+            <div class="w3-half mitadder">
+                <h2>CDMX</h2>
+                Av. Gustavo Baz 209 int. 203 <br>
+                Col. Hacienda Echegaray, Naucalpan <br> 
+                01 (55) 5009 5585
+            </div>
+        </div>
+    </section>
+
+    <?php include("footer.html"); ?>
+</body>
+</html>
