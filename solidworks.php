7d87e433ae20aa4228d4f85a917704df0a534379
Carlos <carlosejimenezh95@gmail.com>
2022-02-22 09:39:46 -0600

    cambiar variable para activar header

--- solidworks.php
+++ solidworks.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
-    <?php $sec=1; ?>
+    <?php $sec=2; ?>
     <?php include("header.php"); ?>
     <div class="banner-principal" style="height: 600px; background-color:aliceblue">
         <div class="contenido">
