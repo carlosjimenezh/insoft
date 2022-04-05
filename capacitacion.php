b70f016fac5ec4124ba7147fdd2c30ddd8181aa7
Carlos <carlosejimenezh95@gmail.com>
2022-04-05 12:59:46 -0500

    crear archivo capacitacion.php

---
+++ capacitacion.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Capacitación</title>
+    <link rel="stylesheet" href="w3pro.css">
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <?php include('header.php'); ?>
+
+    <?php include('footer.html'); ?>
+</body>
+</html>
