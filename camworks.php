c8d0144aa84c24991672e69581cf66c88cc8e7ac
Carlos <carlosejimenezh95@gmail.com>
2022-04-13 11:46:09 -0500

    agregar recuadro de camworks

--- camworks.php
+++ camworks.php
@@ -10,7 +10,29 @@
 <body>
     <?php $sec=3; ?>
     <?php include('header.php'); ?>
-    
+
+    <section class="contenido" style="max-width: 1350px">
+        <div style="border: 1px solid black;margin-top:200px">
+            <div class="contenido w3-row">
+                <div class="w3-half mitadizq w3-hide-medium w3-hide-large" style="margin-bottom: 21px;">
+                    <img src="assets/rectangle.png" alt="">
+                </div>
+                <div class="w3-half mitadizq">
+                    <h2>CAD MECÁNICO EN 3D</h2>
+                    <h2><strong>STANDAR</strong></h2>
+                    Ofrece acceso a todas las herramientas necesarias para realizar superficies complejas, 
+                    chapas desplegadas de chapa metálica y ensamblajes soldados estructurales. Además, incluye 
+                    asistentes para automatizar los diseños, realizar análisis de tensión y determinar el 
+                    impacto medioambiental de los componentes. <br>
+                    <a href="" class="btn">Más información</a>
+                </div>
+                <div class="w3-half mitadder w3-hide-small">
+                    <img src="assets/rectangle.png" alt="">
+                </div>
+                <img src="assets/circle.svg" alt="" class="flotante" width="100px">
+            </div>
+        </div>
+    </section>
 
     <?php include('footer.html'); ?>
 </body>
