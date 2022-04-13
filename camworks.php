6b93ef0c0f406e7c6f8b9513977815096dcc202f
Carlos <carlosejimenezh95@gmail.com>
2022-04-13 10:02:12 -0500

    crear archivo camworks.php

---
+++ camworks.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Camwork</title>
+    <link rel="stylesheet" href="w3pro.css">
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <?php $sec=3; ?>
+    <?php include('header.php'); ?>
+    
+
+    <?php include('footer.html'); ?>
+</body>
+</html>
