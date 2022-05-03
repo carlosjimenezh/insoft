b3b7f69fa482901db68682fbcf4977548d1ca959
Carlos <carlosejimenezh95@gmail.com>
2022-05-03 11:30:27 -0500

    agregar formulario a especialidades

--- capacitacion.php
+++ capacitacion.php
@@ -244,7 +244,7 @@
                 Contamos con cursos desde nivel básico y más de 15 especialidades que te ayudarán a realizar tu 
                 trabajo de manera exitosa, cumpliendo las demandas del mercado laboral actual."
             </div>
-            <div class="w3-row centrar-small">
+            <main class="w3-row centrar-small">
                 <div class="w3-half">
                     <div class="w3-col s3">
                         <img class="imgmax" src="assets/circle.svg" alt="" width="100px">
@@ -509,6 +509,35 @@
                         Lunes a Viernes: 9 AM a 7 PM
                     </div>
                 </div>
+            </main>
+        </div>
+        <h3 class="contenido w3-center">Contacta con el área de ventas</h3>
+        <form action="solidworks.php?a=2#scontacto" class="contenido" name="contacto" id="contacto" method="POST">
+            <div class="w3-row">
+                <div class="w3-half mitadizq">
+                    <input type="text" name="nombre" placeholder="Nombre completo">
+                    <input type="tel" name="telefono" placeholder="Teléfono">
+                    <input type="emal" name="email" placeholder="E-mail">
+                </div>
+                <div class="w3-half mitadder">
+                    <input type="text" name="ciudad" placeholder="Ciudad/País">
+                    <input type="text" name="empresa" placeholder="Empresa o particular">
+                    <input type="text" name="producto" placeholder="Diseño de producto">
+                </div>
+            </div>
+            <div class="checkboxs">
+                <input type="checkbox" name="capacitacion" id="chkcapacitacion">
+                &nbsp; &nbsp;
+                <label for="chkcapacitacion">CAPACITACIÓN</label> <br>
+                <input type="checkbox" name="licencias" id="chklicencias">
+                &nbsp; &nbsp;
+                <label for="chklicencias">IMPLEMENTAR LICENCIAS</label>
+            </div>
+        </form>
+        <div class="lnazul">
+            <div class="flotante" style="background:white; right:50%; transform:translateX(50%);
+            height:10px; width:240px">
+                <a onclick="verifica()" class="btn flotante">Enviar</a>
             </div>
         </div>
     </section>
