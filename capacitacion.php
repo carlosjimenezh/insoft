be1d5ac5bcaf48d324d214d165fb8c051b4f91dc
Carlos <carlosejimenezh95@gmail.com>
2022-05-04 11:35:26 -0500

    quitar la clase centrar-small de section

--- capacitacion.php
+++ capacitacion.php
@@ -28,8 +28,8 @@
         realizar tu trabajo de manera exitosa, cumpliendo las demandas del mercado laboral actual."
     </section>
 
-    <section id="cursos" class="centrar-small">
-        <div class="w3-row contenido" style="max-width: 900px">
+    <section id="cursos">
+        <div class="w3-row contenido centrar-small" style="max-width: 900px">
             <div class="cflex">
                 <div class="w3-col m3 w3-center">
                     <img src="assets/circle.svg" alt="" width="100px">
