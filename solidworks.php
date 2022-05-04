37a4f59d535f863be42dc4b38ffc4933a1b4b8f5
Carlos <carlosejimenezh95@gmail.com>
2022-05-04 10:55:06 -0500

    separar código del verificador del formulario

--- solidworks.php
+++ solidworks.php
@@ -6,28 +6,7 @@
     <title>Solidworks</title>
     <link rel="stylesheet" href="w3pro.css">
     <link rel="stylesheet" href="estilo.css">
-    <script type="text/javascript">
-        function verifica()
-        {
-            if(document.contacto.nombre.value.trim()==""  || document.contacto.email.value.trim()=="" || document.contacto.producto.value.trim()=="")
-            {
-                alert("Es necesario escribir tu nombre, e-mail y el diseño de producto");     
-            }
-            else
-            {
-                var s = document.contacto.email.value;
-                var filter=/^[A-Za-z][A-Za-z0-9_.ñÑ-]*@[A-Za-z0-9_ñÑ-]+\.[A-Za-z0-9_.ñÑ-]+[A-za-z]$/;
-                if(filter.test(s))
-                {            
-                    document.contacto.submit();
-                }
-                else
-                {
-                    alert("Por favor, escribe una dirección de e-mail válida");
-                }
-            }
-        }
-	</script>
+    <script src="js/verificar-formulario.js"></script>
 </head>
 <body>
     <?php $sec=2; ?>
