a75074840fec81d90a7b7b5bacd2f5a713aa6676
Carlos <carlosejimenezh95@gmail.com>
2022-05-05 12:23:36 -0500

    crear archivos con la plantilla base

---
+++ contacto.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+    <link rel="stylesheet" href="w3pro.css">
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <?php
+        $sec = 6; 
+        include('header.php'); 
+    ?>
+
+
+    
+
+
+    <?php include('footer.html'); ?>
+</body>
+</html>
