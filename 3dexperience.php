349353dcab15abf74ac8ce70d250118b6f9ffacb
Carlos <carlosejimenezh95@gmail.com>
2022-05-06 11:51:36 -0500

    creación de matriz de descargas

--- 3dexperience.php
+++ 3dexperience.php
@@ -13,10 +13,112 @@
         include('header.php'); 
     ?>
 
-
+    <section id='matriz' class="contenido" style="margin-top: 150px;">
+        <table>
+            <tr>
+                <td></td>
+                <td>prueba</td>
+                <td>prueba</td>
+                <td>prueba</td>
+            </tr>
+            <tr>
+                <th>FUNCIONES CAD MECÁNICO</th>
+            </tr>
+            <tr>
+                <td>
+                    SWIFT
+                </td>
+                <td>prueba</td>
+                <td>prueba</td>
+                <td>prueba</td>
+            </tr>
+            <tr>
+                <td>
+                    Empezar a trabajar
+                </td>
+                <td>prueba</td>
+                <td>prueba</td>
+                <td>prueba</td>
+            </tr>
+            <tr>
+                <th>FUNCIONES DE GESTIÓN DE DATOS</th>
+            </tr>
+            <tr>
+                <td>
+                    Acceso Seguro
+                </td>
+                <td>prueba</td>
+                <td>prueba</td>
+                <td>prueba</td>
+            </tr>
+            <tr>
+                <td>
+                    Control de Revisiones
+                </td>
+                <td>prueba</td>
+                <td>prueba</td>
+                <td>prueba</td>
+            </tr>
+            <tr>
+                <th>FUNCIONES DE VALIDACION DE DATOS</th>
+            </tr>
+            <tr>
+                <td>
+                    Validación de tolerancia al apilado
+                </td>
+                <td>prueba</td>
+                <td>prueba</td>
+                <td>prueba</td>
+            </tr>
+            <tr>
+                <td>
+                    Simulación de mecanismo completamente integrado
+                </td>
+                <td>prueba</td>
+                <td>prueba</td>
+                <td>prueba</td>
+            </tr>
+            <tr>
+                <th>GESTIÓN DE DATOS</th>
+            </tr>
+            <tr>
+                <td>
+                    Acceso seguro
+                </td>
+                <td>prueba</td>
+                <td>prueba</td>
+                <td>prueba</td>
+            </tr>
+            <tr>
+                <td>
+                    Control de revisiones
+                </td>
+                <td>prueba</td>
+                <td>prueba</td>
+                <td>prueba</td>
+            </tr>
+            <tr>
+                <th>VALIDACIÓN DE DISEÑOS</th>
+            </tr>
+            <tr>
+                <td>
+                    Validación de tolerancia al apilado
+                </td>
+                <td>prueba</td>
+                <td>prueba</td>
+                <td>prueba</td>
+            </tr>
+            <tr>
+                <td>
+                    Simulación de ensamblajes
+                </td>
+                <td>prueba</td>
+                <td>prueba</td>
+                <td>prueba</td>
+            </tr>
+        </table>
+    </section>
     
-
-
     <?php include('footer.html'); ?>
 </body>
 </html>
