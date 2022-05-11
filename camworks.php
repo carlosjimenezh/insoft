59cba0b4adae430734c5e79f68dbdc42995f7460
Carlos <carlosejimenezh95@gmail.com>
2022-05-11 09:50:29 -0500

    cambiar margin-top de camworks

--- camworks.php
+++ camworks.php
@@ -16,7 +16,7 @@
     <?php include('header.php'); ?>
 
     <section id="cam" class="contenido">
-        <div style="border: 1px solid #6C6C6C;margin-top:200px">
+        <div style="border: 1px solid #6C6C6C">
         <h2 class="contenido w3-hide-small">SOFTWARE <br> <strong>CAM PARA CNC</strong> </h2>
             <div class="contenido w3-row" style="position: relative;">
                 <div class="w3-half mitadder w3-hide-medium w3-hide-large">
