f0f18388d1ae862fbc5272735ce1307f4b47b1ab
Carlos <carlosejimenezh95@gmail.com>
2022-05-14 12:38:01 -0500

    cambiar a función sin parametros

--- js/verificar-formulario.js
+++ js/verificar-formulario.js
@@ -1,18 +1,18 @@
-function verifica(idContacto)
+function verifica()
         {
-            if(document.getElementById(idContacto).nombre.value.trim()==""  
-            || document.getElementById(idContacto).email.value.trim()=="" 
-            || document.getElementById(idContacto).producto.value.trim()=="")
+            if(document.contacto.nombre.value.trim()==""  
+            || document.contacto.email.value.trim()=="" 
+            || document.contacto.producto.value.trim()=="")
             {
                 alert("Es necesario escribir tu nombre, e-mail y el diseño de producto");     
             }
             else
             {
-                var s = document.getElementById(idContacto).email.value;
+                var s = document.contacto.email.value;
                 var filter=/^[A-Za-z][A-Za-z0-9_.ñÑ-]*@[A-Za-z0-9_ñÑ-]+\.[A-Za-z0-9_.ñÑ-]+[A-za-z]$/;
                 if(filter.test(s))
                 {            
-                    document.getElementById(idContacto).submit();
+                    document.contacto.submit();
                 }
                 else
                 {
