a2555c876de2789760f5741e5386a88c3273d488
Carlos <carlosejimenezh95@gmail.com>
2022-06-06 11:06:28 -0500

    crear sección de contacto

--- nosotros.php
+++ nosotros.php
@@ -13,9 +13,25 @@
         include('header.php'); 
     ?>
 
-
+    <section id="nosotros">
+        <div class="banner-principal" style="height: 600px; background-image:url(assets/area-ventas.jpg)">
+            <div class="contenido" style="margin-top: 0;height:100%; position:relative"> 
+                <div class="w3-display-middle w3-center">
+                    <span>NOSOTROS</span> <br>
+                    <img src="assets/logo-solidworks.svg" class="logo" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="w3-row contenido"> 
+            <div class="w3-half w3-center">
+                prueba
+            </div>
+            <div class="w3-half w3-center">
+                prueba
+            </div>
+        </div>
+    </section>
     
-
 
     <?php include('footer.html'); ?>
 </body>
