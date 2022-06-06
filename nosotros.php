b7976ca62f72551ce1336103b20bfa42521f32b0
Carlos <carlosejimenezh95@gmail.com>
2022-06-06 11:28:49 -0500

    crear sección de mision y vision

--- nosotros.php
+++ nosotros.php
@@ -23,11 +23,17 @@
             </div>
         </div>
         <div class="w3-row contenido"> 
-            <div class="w3-half w3-center">
-                prueba
+            <div class="w3-half w3-center mitadizq">
+                <div style="max-width: 400px; margin:auto">
+                    <h2><strong>MISIÓN</strong></h2>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione accusamus in laborum voluptatum quidem, neque quod numquam voluptates atque similique ducimus totam explicabo quas commodi? Vero accusamus distinctio aut voluptatum.
+                </div>
             </div>
-            <div class="w3-half w3-center">
-                prueba
+            <div class="w3-half w3-center mitadder">
+                <div style="max-width: 400px; margin:auto">
+                    <h2><strong>VISIÓN</strong></h2>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique consectetur placeat vel esse ducimus rerum sit eos officiis assumenda nobis nostrum molestias ab pariatur nihil perspiciatis, nemo sed aperiam tempore!
+                </div>
             </div>
         </div>
     </section>
