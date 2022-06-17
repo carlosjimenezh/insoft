a5f8dce48ba52f475d03c7df0a8aa351372cdd1a
Carlos <carlosejimenezh95@gmail.com>
2022-06-17 09:00:15 -0500

    cambiar contenido de nosotros

--- nosotros.php
+++ nosotros.php
@@ -22,20 +22,22 @@
                 </div>
             </div>
         </div>
-        <div class="w3-row contenido"> 
-            <div class="w3-half w3-center mitadizq">
-                <div style="max-width: 400px; margin:auto">
-                    <h2><strong>MISIÓN</strong></h2>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione accusamus in laborum voluptatum quidem, neque quod numquam voluptates atque similique ducimus totam explicabo quas commodi? Vero accusamus distinctio aut voluptatum.
-                </div>
-            </div>
-            <div class="w3-half w3-center mitadder">
-                <div style="max-width: 400px; margin:auto">
-                    <h2><strong>VISIÓN</strong></h2>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique consectetur placeat vel esse ducimus rerum sit eos officiis assumenda nobis nostrum molestias ab pariatur nihil perspiciatis, nemo sed aperiam tempore!
-                </div>
-            </div>
-        </div>
+        <section class="contenido w3-center" style="max-width: 900px; margin-bottom:150px; margin-top:100px">
+            <h2> <strong>NOSOTROS</strong></h2>
+            A lo largo de la historia, el hombre ha evolucionado creando herramientas nuevas que mejoren o faciliten las tareas cotidianas en su día a día; de este modo, ejemplos de grandes <b>innovaciones</b> como la invención de la rueda hasta el día de hoy no se detienen.
+            <br> <br>
+            En <b>Grupo Insoft</b> , tenemos más de 20 años trabajando y evolucionando, para mejorar la vida laboral y creativa de toda la industria Mexicana, proporcionando herramientas cada vez más novedosas, tanto en software como capacitación.
+            <br> <br>
+            Estamos conformados por un excelente <b>equipo de profesionales</b>, enfocados en contribuir con nuestros clientes a desarrollar sus proyectos y/o marcas logrando hacer crecer sus negocios con un sistema enfocado en el mercado actual.
+            <br> <br> 
+            Además, nuestro principal objetivo, es mantener el nivel de calidad de un centro integral donde nuestros usuarios de la industria, encuentren múltiples <b>soluciones</b> para facilitar su desarrollo.
+            <br> <br>
+            Nos preocupamos por crear relaciones de confianza entre nuestros clientes y colaboradores para poder encontrar la solución más efectiva a sus demandas.
+            <br> <br>
+            Mantenemos un compromiso por seguir esforzándonos día con día para ayudar a implementar todas las <b>novedades industriales</b> teniendo siempre presente el objetivo claro de la mejora constante.
+            <br> <br>
+            Queremos mostrarte nuestro trabajo, para obtener más información, no dudes en <a href="contacto.php" style="text-decoration: underline"><b>contactarnos</b></a>. 
+        </section>
     </section>
     
 
